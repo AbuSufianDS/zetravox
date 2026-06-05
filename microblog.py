ae1332055212ae264016196a809617b6395ff521
Md. Abu Sufian <mdabusufian1323@gmail.com>
2026-06-06 04:16:01 +0800

    port change

--- microblog.py
+++ microblog.py
@@ -79,6 +79,6 @@ def make_shell_context():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     debug = os.environ.get('FLASK_ENV', 'production') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug)
