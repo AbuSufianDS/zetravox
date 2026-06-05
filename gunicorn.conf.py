26e63bd732e840609e6d8e946473bf4735839ed1
Md. Abu Sufian <mdabusufian1323@gmail.com>
2026-06-06 03:40:44 +0800

    For render run

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 workers = 1
 threads = 2
 worker_class = 'sync'
-max_requests = 100
+max_requests = 50
 max_requests_jitter = 10
-timeout = 120
-keepalive = 5
+timeout = 60
+keepalive = 2
