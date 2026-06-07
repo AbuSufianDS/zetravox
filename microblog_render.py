50f0062a5f2ca0d3a5d61eb6ea235d94baef6aa6
Md. Abu Sufian <mdabusufian1323@gmail.com>
2026-06-08 02:33:51 +0800

    Just delate one unusual file

---
+++ microblog_render.py
@@ -0,0 +1,11 @@
+from app import create_app, db
+from app.models import User, Post, Comment, Notification, Message, Task
+
+app = create_app()
+
+# Reduce memory usage
+import gc
+gc.collect()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
